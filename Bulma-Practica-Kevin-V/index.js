189468a75b1ef053116000a71ee5ce2a3453922a
Toons1013 <107084332+Toons1013@users.noreply.github.com>
2023-10-07 12:15:02 -0600

    Delete Bulma-Practica-Kevin-V directory

--- Bulma-Practica-Kevin-V/index.js
+++
@@ -1,6 +0,0 @@
-const burgerIcon = document.querySelector('#borga');
-const navbarMenu = document.querySelector('#nav-l');
-
-burgerIcon.addEventListener('click', () => {
-    navbarMenu.classList.toggle('is-active')
-})
